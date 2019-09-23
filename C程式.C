26434d0796244f4400f21e067cd582328e64d2e1
R20072127 <55220998+R20072127@users.noreply.github.com>
2019-09-23 15:04:58 +0800

    Create C程式.C

---
+++ C程式.C
@@ -0,0 +1,8 @@
+#include<stdio.h> //匯入函式庫(io)
+#incluele<stdlib.h>//匯入函式庫(lib)
+
+回傳型態int main(void){//主函式(程式進入點)(entrance of the program) main method
+    printf("Hello World\n");//列印出文字.Hello Word並換行 
+    system("pause");//保留畫面 
+    return 0;//回傳值 
+}//主函式結束(end of main method) 
