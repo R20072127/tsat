ad775a0677edd7ca369dc0b5fa12383ded1e506b
R20072127 <55220998+R20072127@users.noreply.github.com>
2019-09-23 16:40:37 +0800

    Create 9月23號C程式語言.C

---
+++ 9月23號C程式語言.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+	int a1 = 1;
+	int a2 = 2;
+	int a3 = 3;
+	int a = a1*a2*a3;
+	printf("%d*%d*%d=%d\n",a1,a2,a3,a);
+	system("pause");
+	return 0;
+}
